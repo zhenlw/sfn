488e8f511f1454e95d1c890a83e6f1272b41c58d
zhenlw <zhenlw@msn.com>
2021-12-30 21:57:03 +0800

    unify the indent style to space-only for sfn_core.cpp

--- sfn/sfn_core.cpp
+++ sfn/sfn_core.cpp
@@ -21,49 +21,49 @@ static void sendKey(WORD code, DWORD scode, DWORD flags)
 static LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if ( nCode < 0 )
-	return CallNextHookEx(NULL, nCode, wParam, lParam);
+        return CallNextHookEx(NULL, nCode, wParam, lParam);
 
     KBDLLHOOKSTRUCT* h = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
     if (h->flags & LLKHF_INJECTED && h->dwExtraInfo == injectedFlag)
-	return CallNextHookEx(NULL, nCode, wParam, lParam);
+        return CallNextHookEx(NULL, nCode, wParam, lParam);
 
     //std::cout << "codes " << h->vkCode << " state " << s_iState << std::endl;
     if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
-	//std::cout << "down" << std::endl;
-	if (h->vkCode == g_act_vk) {
-	    if (s_iState < 2) s_iState++; //on repeat the state will be 2 like with the "other keys presses case below, so we don't send the act key tap on up too, if this happens
-	    return 1;
-	}
+        //std::cout << "down" << std::endl;
+        if (h->vkCode == g_act_vk) {
+            if (s_iState < 2) s_iState++; //on repeat the state will be 2 like with the "other keys presses case below, so we don't send the act key tap on up too, if this happens
+            return 1;
+        }
 
-	if (s_iState != 0) {
-	    //key down case in activated mode
-	    s_iState = 2; //so we don't send the act key tap on up
-	    if (g_kmap[h->vkCode].vkt >0) {
-		//std::cout << "vkt " << g_kmap[h->vkCode].vkt;
-		sendKey(g_kmap[h->vkCode].vkt, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags);
-		g_kmap[h->vkCode].in_mapped_state = 1;
-		return 1;
-	    }
-	}
-	//just go through default routine
+        if (s_iState != 0) {
+            //key down case in activated mode
+            s_iState = 2; //so we don't send the act key tap on up
+            if (g_kmap[h->vkCode].vkt >0) {
+                //std::cout << "vkt " << g_kmap[h->vkCode].vkt;
+                sendKey(g_kmap[h->vkCode].vkt, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags);
+                g_kmap[h->vkCode].in_mapped_state = 1;
+                return 1;
+            }
+        }
+        //just go through default routine
     }
     else {
-	if (h->vkCode == g_act_vk) {
-	    //act key up, send a tap and clean up state
-	    if (s_iState == 1) {
-		sendKey(g_act_vk, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags);
-		sendKey(g_act_vk, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags | KEYEVENTF_KEYUP);
-	    }
-	    s_iState = 0;
-	    return 1;
-	}
-	//whether in act mode, we still look the key up in the list to see what kc to send
-	if (g_kmap[h->vkCode].in_mapped_state) {
-	    sendKey(g_kmap[h->vkCode].vkt, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags | KEYEVENTF_KEYUP);
-	    g_kmap[h->vkCode].in_mapped_state = 0;
-	    return 1;
-	}
-	//otherwise do nothing more than rutine process
+        if (h->vkCode == g_act_vk) {
+            //act key up, send a tap and clean up state
+            if (s_iState == 1) {
+                sendKey(g_act_vk, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags);
+                sendKey(g_act_vk, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags | KEYEVENTF_KEYUP);
+            }
+            s_iState = 0;
+            return 1;
+        }
+        //whether in act mode, we still look the key up in the list to see what kc to send
+        if (g_kmap[h->vkCode].in_mapped_state) {
+            sendKey(g_kmap[h->vkCode].vkt, g_kmap[h->vkCode].scode, g_kmap[h->vkCode].flags | KEYEVENTF_KEYUP);
+            g_kmap[h->vkCode].in_mapped_state = 0;
+            return 1;
+        }
+        //otherwise do nothing more than rutine process
     }
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
